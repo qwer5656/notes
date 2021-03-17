ddbedcbb62c93b6f29f795797ca3b15ad4631058
qwer5656 <determine2014@gmail.com>
2021-03-17 16:36:59 +0800

    Add files via upload

---
+++ dist/js/chunk-2d21a3d2.352a91e1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"home"})},s=[],i={name:"Home",data:function(){return{test:""}},created:function(){this.test=this.$route.params.id},watch:{"$route.params.id":function(t){this.test=t}}},u=i,c=n("2877"),r=Object(c["a"])(u,a,s,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.352a91e1.js.map
